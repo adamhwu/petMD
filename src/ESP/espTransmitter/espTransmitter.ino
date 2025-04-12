2a957a8003d308b02bc3ed7d951558e8f88762ad
Radish <56287985+Rassul-Li@users.noreply.github.com>
2025-04-12 12:59:20 -0700

    yes

--- src/ESP/espTransmitter/espTransmitter.ino
+++ src/ESP/espTransmitter/espTransmitter.ino
@@ -1,3 +1,4 @@
+#include <PulseSensorPlayground.h>
 #include <Wire.h>
 #include <Adafruit_AHTX0.h>
 #include <WiFi.h>
@@ -6,8 +7,11 @@
 const char* ssid = "Adam";
 const char* password = "testing123";
 const char* host = "11.27.2.195";  // IP of your PC running WebSocket server
+const int PulseWire = 4;
+int Threshold = 500;
 
 WebSocketsClient webSocket;
+PulseSensorPlayground pulseSensor;  // Creates an object
 
 // Create a custom I2C instance on GPIO 17 (SDA) and 16 (SCL)
 TwoWire myWire = TwoWire(0);  // Use I2C bus 0 (can also be 1)
@@ -41,6 +45,10 @@ void setup() {
   }
   Serial.println("AHT20 sensor initialized successfully.");
 
+  pulseSensor.analogInput(PulseWire);   
+	pulseSensor.blinkOnPulse(IO38);       // Blink on-board LED with heartbeat
+	pulseSensor.setThreshold(Threshold);   
+
   // wifi setup
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -66,6 +74,7 @@ void loop() {
   // sensors
   sensors_event_t humidity, temp;
   aht.getEvent(&humidity, &temp); // populate temp and humidity objects
+  int myBPM = pulseSensor.getBeatsPerMinute();   
 
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
