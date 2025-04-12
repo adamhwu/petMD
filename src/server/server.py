8289e89c06237b6b6e95f327bfb1b918fe38df0e
adamhwu <wuadam42@gmail.com>
2025-04-12 14:07:33 -0700

    video stream working with sockets

--- src/server/server.py
+++ src/server/server.py
@@ -1,21 +1,36 @@
 # server.py
 import asyncio
+from websockets.asyncio.server import broadcast
 import websockets
+import base64
+import numpy as np
+import cv2
 
-connected_clients = set()
+connected_clients = set() 
 
 async def handler(websocket):
     connected_clients.add(websocket)
     print("Client connected")
+    print(connected_clients)
+
     try:
         async for message in websocket:
-            print(f"Received: {message}")
-            # Echo message back to client
-            await websocket.send(f"Echo: {message}")
+            try:
+                img_data = base64.b64decode(message)
+                np_arr = np.frombuffer(img_data, np.uint8)
+                frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+                broadcast(connected_clients, message)
+
+            except Exception as e:
+                print(f"Error: {e}")
+
     except websockets.ConnectionClosed:
         print("Client disconnected")
+
     finally:
         connected_clients.remove(websocket)
+
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", 8765):
