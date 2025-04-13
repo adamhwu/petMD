761868259988f171d5348ca80ec2a99b2fcdcf9c
adamhwu <wuadam42@gmail.com>
2025-04-12 20:46:26 -0700

    finishing video stream and vitals

--- src/ESP/espTransmitter/espTransmitter.ino
+++ src/ESP/espTransmitter/espTransmitter.ino
@@ -1,80 +1,47 @@
 #include <PulseSensorPlayground.h>
 #include <Wire.h>
 #include <Adafruit_AHTX0.h>
-#include <WiFi.h>
-#include <WebSocketsClient.h>
 
-const char* ssid = "Adam";
-const char* password = "testing123";
-const char* host = "11.27.2.195";  // IP of your PC running WebSocket server
-const int PulseWire = 4;
-int Threshold = 500;
-
-WebSocketsClient webSocket;
+// pulse sensor object
+const int PULSE_INPUT = 7;
+const int THRESHOLD = 550;   
 PulseSensorPlayground pulseSensor;  // Creates an object
 
 // Create a custom I2C instance on GPIO 17 (SDA) and 16 (SCL)
 TwoWire myWire = TwoWire(0);  // Use I2C bus 0 (can also be 1)
 Adafruit_AHTX0 aht;
 
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch(type) {
-    case WStype_CONNECTED:
-      Serial.println("Connected to server");
-      webSocket.sendTXT("Hello from ESP32");
-      break;
-    case WStype_TEXT:
-      Serial.printf("Received: %s\n", payload);
-      break;
-    case WStype_DISCONNECTED:
-      Serial.println("Disconnected");
-      break;
-  }
-}
-
 void setup() {
-  Serial.begin(9600);
-  Serial.println("we here");
+  Serial.begin(115200);
   delay(10);
+
   // Initialize custom I2C pins
   myWire.begin(17, 16);  // SDA, SCL
-  // Pass the custom Wire instance to the sensor
+
+  // pulseSensor values
+  pulseSensor.analogInput(PULSE_INPUT);   
+	pulseSensor.setThreshold(THRESHOLD); 
+
+  if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
+
+  // check temp sensor connection
   if (!aht.begin(&myWire)) {
     Serial.println("Could not find AHT20 sensor, check wiring!");
     while (1) delay(10);
   }
   Serial.println("AHT20 sensor initialized successfully.");
-
-  pulseSensor.analogInput(PulseWire);   
-	pulseSensor.blinkOnPulse(IO38);       // Blink on-board LED with heartbeat
-	pulseSensor.setThreshold(Threshold);   
-
-  // wifi setup
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to WiFi");
-
-  webSocket.begin("11.27.2.195", 8080, "/");  // use "/" instead of "/ws"  webSocket.setReconnectInterval(5000);
 }
 
 void loop() {
-  webSocket.loop();
-
-  // wifi
-  // Example: send something every 2s
-  static unsigned long lastSend = 0;
-  if (millis() - lastSend > 2000) {
-    webSocket.sendTXT("Sensor data: 123");
-    lastSend = millis();
-  }
-
   // sensors
   sensors_event_t humidity, temp;
+  int myBPM;
   aht.getEvent(&humidity, &temp); // populate temp and humidity objects
-  int myBPM = pulseSensor.getBeatsPerMinute();   
+  if (pulseSensor.sawStartOfBeat()) {
+    myBPM = pulseSensor.getBeatsPerMinute();
+  }
 
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
@@ -84,5 +51,9 @@ void loop() {
   Serial.print(humidity.relative_humidity);
   Serial.println(" %");
 
-  delay(1000); // update every 2 seconds
+  Serial.print("Heartbeat: ");
+  Serial.print(myBPM);
+  Serial.println(" BPM");
+
+  delay(1000); // update every 1 seconds
 }
--- src/server/server.py
+++ src/server/server.py
@@ -5,6 +5,7 @@ import websockets
 import base64
 import numpy as np
 import cv2
+import json
 
 connected_clients = set() 
 
@@ -12,18 +13,27 @@ async def handler(websocket):
     connected_clients.add(websocket)
     print("Client connected")
     print(connected_clients)
+    package = {}
 
     try:
         async for message in websocket:
-            try:
-                img_data = base64.b64decode(message)
-                np_arr = np.frombuffer(img_data, np.uint8)
-                frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-                broadcast(connected_clients, message)
-
-            except Exception as e:
-                print(f"Error: {e}")
+            # data = json.loads(message)
+            # if isinstance(data, str):
+            #     data = json.loads(data)
+            #
+            # if data["type"] == "sensor":
+            #     package = {
+            #         "type": "sensor", 
+            #         "temperature": data["temperature"],
+            #         "humidity": data["humidity"],
+            #         "pulse": data["pulse"]
+            #     }
+            #     json_msg = json.dumps(package)
+            #
+            # else:
+            #     json_msg = json.dumps(data)
+            #
+            broadcast(connected_clients, message)
 
     except websockets.ConnectionClosed:
         print("Client disconnected")
