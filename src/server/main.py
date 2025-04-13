999aba893ea4115d68f16713d5eba8894f577c26
adamhwu <wuadam42@gmail.com>
2025-04-12 23:18:18 -0700

    main.py and requirements for hosting

---
+++ src/server/main.py
@@ -0,0 +1,26 @@
+# main.py
+import asyncio
+from websockets.asyncio.server import broadcast
+import websockets
+
+connected_clients = set() 
+
+async def handler(websocket):
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+           broadcast(connected_clients, message)
+
+    except websockets.ConnectionClosed:
+        print("Client disconnected")
+
+    finally:
+        connected_clients.remove(websocket)
+
+async def main():
+    async with websockets.serve(handler, "", 10000):
+        print("WebSocket server started")
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
